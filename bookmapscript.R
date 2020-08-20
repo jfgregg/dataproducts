f60c3ad38385d8e41113f0c6813ceb2573bbd1aa
jfgregg <jofrgregg@protonmail.com>
2020-08-20 23:09:05 +0200

    Created html file with date

--- bookmapscript.R
+++ bookmapscript.R
@@ -3,13 +3,16 @@ library(dplyr)
 
 bookdata <- read.csv("./bookmap.csv")
 bookdata <- as.data.frame(bookdata)
-bookicon <- makeIcon(iconUrl = "./book.png",iconWidth = 31*215/230, iconHeight = 31,iconAnchorY = 31*215/230/2, iconAnchorX = 16 )
 
 popup <- paste('<b>',bookdata$title, "</b> <br> <em>",bookdata$author,  "</em> <br> ",bookdata$review, " <br> <b>",bookdata$by," <b>")
 my_map <- data.frame(lng = bookdata$lng,lat = bookdata$lat)
 
+print("20th August 2020")
 my_map %>%
   leaflet() %>%
   addTiles()  %>%
-  addMarkers(clusterOptions = markerClusterOptions(), popup = popup, icon = bookicon)
+  addMarkers(clusterOptions = markerClusterOptions(), popup = popup) %>%
+
+
+  
 
