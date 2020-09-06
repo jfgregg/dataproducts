94c6bf69d7fd4c955a17d30667a455359d6bc19a
jfgregg <jofrgregg@protonmail.com>
2020-09-06 14:25:57 +0200

    AIR Shiny app

--- AIRCalculator/server.R
+++ AIRCalculator/server.R
@@ -8,19 +8,85 @@
 #
 
 library(shiny)
+library(dplyr)
+library(plotly)
+library(ggplot2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    
+    model <- reactive({
+        
+            time <- NULL
+            
+            # calculate the volume of the room
+            vol  <- input$Area*input$Height
+            
+            # set the IVRR
+            IVRR <- 1.4
+            
+            # Based on activity select Eq and Iq
+            if(input$Activity == "Resting"){
+                EQ = 9.49
+                IQ = 0.49
+            }
+            if(input$Activity == "Standing"){
+                EQ = 11.5
+                IQ = 0.54
+            }
+            if(input$Activity == "Light Exercise"){
+                EQ = 26.5
+                IQ = 1.38
+            }
+            if(input$Activity == "Heavy Exercise"){
+                EQ = 63.7
+                IQ = 3.3
+            }
+            
+            #Expected number of infected people
+            expinf <- input$Occupants*(input$Prob/100000)*(1-input$Prob/100000)
+            
+            time <- seq(0,8,by = 1/60)
+            
+            model <- data.frame(time) %>%
+                mutate(quanta = (EQ/(IVRR*vol))*(1-exp(-IVRR*time))) %>%
+                mutate(totalquanta = quanta*expinf) %>%
+                mutate(ndt = totalquanta/60) %>%
+                mutate(cumndt = cumsum(ndt)) %>%
+                mutate(risk = 15*(1-exp(-IQ*cumndt)))
+    })
+            
+    output$riskplot <- renderPlotly({
+                
+        # draw the risk versus time graph
+        g <- ggplot(data = model(), aes(x = time, y = risk))
+        g <- g + geom_point(colour = "red", shape = 4, size = 0.1)
+        g <- g + xlab("Time (hours)") + ylab("Risk %")
+        g <- g + geom_vline(xintercept = input$duration/60, linetype = "dashed")
+        ggplotly(g)
     })
 
+    output$timerisk <- renderText({
+        
+        timestep <- input$duration + 1
+        risk <- round(model()$risk[timestep],3)
+        timerisk <- paste("The risk after", as.character(input$duration), "minutes is ", as.character(risk), "%")
+    })
+
+    output$tenrisk <- renderText({
+        
+        timestep <- input$duration + 1
+        risk <- round(1 - pbinom(prob = model()$risk[timestep],0,size = 10),3)
+        tenrisk <- paste("The risk of infection if you do this activity 10 times is", as.character(risk), "%")
+    })
+    
+    output$oneprisk <- renderText({
+        
+        timestep <- input$duration + 1
+        onep <- round(log(0.99, base = (1-model()$risk[timestep]/100)),0)
+        oneprisk <- paste("In order to have a risk of infection of 1% you must do this activity", onep, "times")
+    })
+    
+       
 })
--- AIRCalculator/ui.R
+++ AIRCalculator/ui.R
@@ -8,26 +8,60 @@
 #
 
 library(shiny)
+library(plotly)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Airborne Infection Risk"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
+            numericInput("Area",
+                        "Room Area (m^2):",
                         min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
+                        max = 2000,
+                        value = 200),
+            numericInput("Height",
+                         "Cieling Height (m):",
+                         min = 1,
+                         max = 10,
+                         value = 3),
+            numericInput("Occupants",
+                         "Number of Occupants:",
+                         min = 1,
+                         max = 200,
+                         value = 20, 
+                         step = 1),
+            sliderInput("Prob",
+                         "Current infection rate (per 100,000 people):",
+                         min = 0,
+                         max = 10000,
+                         value = 100, 
+                         step = 1),
+            selectInput("Activity",
+                        "Activty of Participants",
+                        choices = c("Resting", "Standing", "Light Exercise", "Heavy Exercise"), 
+                        selected = "Resting"),
+            numericInput("duration",
+                         "Length of Activity (mins):",
+                         min = 1,
+                         max = 480,
+                         value = 60)
+            ),
+    
+                     
+                     
+        # Show a plot of the risk over time
         mainPanel(
-            plotOutput("distPlot")
+            tabsetPanel(type = "tabs",
+                        tabPanel("Risk Plot", br(), plotlyOutput("riskplot")),
+                        tabPanel("Repeated Risks", br(), textOutput("timerisk"), 
+                                 textOutput("tenrisk"),
+                                 textOutput("oneprisk"))
+            )
         )
-    )
+    ) 
 ))
